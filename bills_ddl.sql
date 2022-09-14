d00a14d0a5d5b4b89ea59774de13641cfc28467f
MeenakshiMathur <112808212+MeenakshiMathur@users.noreply.github.com>
2022-09-14 12:30:05 +0530

    Add files via upload

---
+++ bills_ddl.sql
@@ -0,0 +1,19 @@
+ 
+
+CREATE TABLE Bills(
+--This table holds billing data1 
+BillsID INT Identity(1,1) PRIMARY KEY,
+OrderID INT Not nUll FOREIGN KEY REFERENCES order_table (OrderID),
+RestaurantID INT Not Null FOREIGN KEY REFERENCES Restaurant (RestaurantID),
+BillAmount FLOAT Not Null ,
+CustomerID INT Not Null FOREIGN KEY REFERENCES customer 
+
+)
+
+CREATE NONCLUSTERED INDEX NC_Bills_RestaurantID on Bills (RestaurantID) INCLUDE (BillAmount)
+
+
+CREATE NONCLUSTERED INDEX NC_Bills_OrderID on Bills (OrderID) INCLUDE (BillAmount)
+
+CREATE NONCLUSTERED INDEX NC_Bills_CustomerID on Bills (CustomerID) INCLUDE (BillAmount)
+
